f4d64bd0bcefc51db990553fed420de59791a527
priyanshu Jha <priyanshujha14@gmail.com>
2022-08-11 20:52:37 +0530

    first commit

---
+++ ohyes/src/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./nav.css";
+function Nav() {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://toppng.com/public/uploads/thumbnail/my-design-oh-yes-11563540485fa1wdw3ayu.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ ohyes/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBLfMXlEiQCPcEjknacQ2vhfH2QfV-LLEU",
+  authDomain: "ohyes-c1cd2.firebaseapp.com",
+  projectId: "ohyes-c1cd2",
+  storageBucket: "ohyes-c1cd2.appspot.com",
+  messagingSenderId: "207373220753",
+  appId: "1:207373220753:web:1c3ed95559eb0655993830",
+  measurementId: "G-B9XC4X9FBK",
+};
